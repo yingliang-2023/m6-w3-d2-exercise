77bfed7dae5973ce7eb4d0d10a2517bad3786004
YING LIANG <y.liang.168@westcliff.edu>
2023-06-20 21:58:41 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route,Link,Outlet} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <nav style = {{margin:10}}>
+      <Link to='/' style={{padding:5}}>
+        Home
+      </Link>
+      <Link to='/about' style={{padding:5}}>
+        About
+      </Link>
+    </nav>
+   {
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
+   }</Router>
   );
 }
 
+
+//Home
+function Home(){
+  return (
+    <div style={{padding:20}}>
+    <h2>Home View</h2>
+    <p>Nothing herefasdfafawe,faewgaerg</p>
+    </div>
+  )
+};
+
+
+//About
+function About(){
+  return (
+    <div style={{padding:20}}>
+    <h2>About View</h2>
+    <p>Nothing here fawe, afwege</p>
+
+    </div>
+  )
+};
 export default App;
